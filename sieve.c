3ee1acc65fc9927efe27a8e7116d2ccfed473c30
Zer0-M <mjamil90@stuy.edu>
2018-10-20 16:36:36 -0400

    sieve now finds larger values

--- sieve.c
+++ sieve.c
@@ -4,29 +4,26 @@
 #include "sieve.h"
 
 int siv_of_E(int n){
-    char bucket[150000];
-    int nth_prime;
-    for(int i=0;i<sizeof(bucket)/sizeof(char);i++){
-        bucket[i]=0;
-    }
-    int count=0;
-    for(int i=2;i<sizeof(bucket)/sizeof(char);i++){
-        int prime;
+    int size=(int)(n*log((double)n)*1.15);
+    char * bucket=calloc(size,sizeof(char *));
+    int count=1;
+    for(int i=3;i<size;i+=2){
         if(!bucket[i]){
-            prime=i;
+            //printf("Prime %d : %d\n",count,i);
             count++;
             if(count==n){
-                nth_prime=i;
+                return i;
             }
-            for(int j=i+1;j<sizeof(bucket)/sizeof(char);j++){
-                if(j%prime==0){
-                    bucket[j]=1;
+            if(i<(n/10)){
+                for(int j=i*3;j<size;j+=i){
+                    if(!bucket[j]&&j%i==0){
+                        bucket[j]=1;
+                    }
                 }
             }
         }        
     }
-    return nth_prime;
 }
 int main(){
-    printf("%d\n",siv_of_E(10000));
+    printf("%d\n",siv_of_E(1000000));
 }
--- sieve.h
+++ sieve.h
@@ -1,1 +1,2 @@
 int siv_of_E(int n);
+int sieve(int targ);
