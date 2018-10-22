dec3cd9c22e680c1aff9af5103538473a5724369
Zer0-M <mjamil90@stuy.edu>
2018-10-21 22:59:01 -0400

    faster sieve

--- sieve.c
+++ sieve.c
@@ -1,16 +1,10 @@
 #include "sieve.h"
 
 void bitchange(int * bitarray,int bitindex){
-    int bit=bitindex%32;
-    int index=bitindex/32;
-    int final=1 << bit;
-    bitarray[index]= bitarray[index]|final;
+    bitarray[bitindex/32]= bitarray[bitindex/32]|1 << bitindex%32;
 }
 int bitcmp(int * bitarray,int bitindex ){
-    int bit=bitindex%32;
-    int index=bitindex/32;
-    int final=1 << bit;
-    return bitarray[index]&final;
+    return bitarray[bitindex/32]&1 << bitindex%32;
 }
 int sieve(int n){
     int size;
@@ -20,10 +14,10 @@ int sieve(int n){
     else{
         size=(int)(n*log((double)n)*1.15);
     }
-    int * bucket=calloc((size/32)+1,sizeof(char *));
+    int * bucket=calloc((size/32),sizeof(char *));
     int count=1;
     for(int i=3;i<size;i+=2){
-        if(!bitcmp(bucket,i)){
+        if(!(bucket[i/32]&1 << i%32)){
             //printf("Prime %d : %d\n",count,i);
             count++;
             if(count==n){
@@ -31,9 +25,9 @@ int sieve(int n){
                 return i;
             }
             if(n<100||i<(n/10)){
-                for(int j=i*3;j<size;j+=i){
-                    if(!bitcmp(bucket,j)&&j%i==0){
-                        bitchange(bucket,j);
+                for(int j=i*i;j>0&&j<size;j+=i){
+                    if(!(bucket[j/32]&1 << j%32)&&j%i==0){
+                         bucket[j/32]= bucket[j/32]|1 << j%32;
                     }
                 }
             }
@@ -41,7 +35,7 @@ int sieve(int n){
     }
 }
 
-/*int siv_of_E(int n){
+/*int sieve(int n){
     int size=(int)(n*log((double)n)*1.15);
     char * bucket=calloc(size,sizeof(char *));
     int count=1;
@@ -52,8 +46,8 @@ int sieve(int n){
             if(count==n){
                 return i;
             }
-            if(i<(n/10)){
-                for(int j=i*3;j<size;j+=i){
+            if(i*i>0&&i*i<size&&i<(n/10)){
+                for(int j=i*i;j<size;j+=i){
                     if(!bucket[j]&&j%i==0){
                         bucket[j]=1;
                     }
