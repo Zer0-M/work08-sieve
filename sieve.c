d416870f5a59e35c06d7ecd194abd8effab0aedf
Zer0-M <mjamil90@stuy.edu>
2018-10-23 22:50:45 -0400

    sieve improved, but commented out to ensure functionality

--- sieve.c
+++ sieve.c
@@ -1,36 +1,39 @@
 #include "sieve.h"
 
 
-/*int sieve(int n){
+int sieve(int n){
     int size;
     if(n<1000){
         size=n*10;
     }
     else{
-        size=(int)(n*log((double)n)*1.15);
+        size=(int)(n*log((double)n)*1.15)/2;
     }
-    int count=n;
-    unsigned int * bucket=malloc((size/32)*sizeof(int *));
-    for(int i=3;i<size;i+=2){
-        if(!(bucket[i/32]&1 << i%32)){
-            //printf("Prime %d : %d\n",count,i);
-            count--;
-            if(count==1){
-                free(bucket);
-                return i;
-            }
-            if(i*i>0){
-                for(int j=i*i;j<size;j+=i){
-                    if(j%2!=0&&j%i==0){
-                         bucket[j/32]= bucket[j/32]|1 << j%32;
-                    }
+    int count=1;
+    int * bucket=calloc(size/32,sizeof(int));
+    int start=3;
+    int ind=0;
+    for(;start<=(int)sqrt(2*size);start+=2){
+        if(!(bucket[ind/32]&1 << ind%32)){
+                for(int j=ind+start+start;j<size;j+=start){
+                    bucket[j/32]|= 1 << j%32;
                 }
-            }
-        }        
+        }
+        ind++;        
     }
-}*/
+    start=3;
+    for(int i=0;i<size;i++){
+        if(!(bucket[i/32]&1 << i%32)){
+            count++;
+        }
+        if(count==n){
+            return start;
+        }
+        start+=2;
+    }
+}
 
-int sieve(int n){
+/*int sieve(int n){
     int size;
     if(n<1000){
         size=n*10;
@@ -56,4 +59,4 @@ int sieve(int n){
             }
         }        
     }
-}
+}*/
